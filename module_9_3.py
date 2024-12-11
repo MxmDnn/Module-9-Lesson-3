edcf66e3f1a60aef7894c14d9565274efb96b2c9
MxmDnn <mxmjuk@gmail.com>
2024-12-11 08:49:12 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -4,6 +4,7 @@ second = ['Строка', 'Урбан', 'Компьютер']
 first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
 print(list(first_result))
 
-second_result = (len(first[i]) == len(second[i]) for i in range(0,3))
-print(list(second_result))
+# second_result = (len(first[i]) == len(second[i]) for i in range(0,3))
 
+second_result = (len(first[i]) == len(second[i]) for i in range(len(first)))
+print(list(second_result))
