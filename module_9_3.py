f9b04049e182cd5475e39aa96ece34308fcffe8e
MxmDnn <mxmjuk@gmail.com>
2024-12-11 09:16:47 +0300

    Update module_9_3.py

--- module_9_3.py
+++ module_9_3.py
@@ -4,8 +4,9 @@ second = ['Строка', 'Урбан', 'Компьютер']
 first_result = (len(x) - len(y) for x, y in zip(first, second) if len(x) != len(y))
 print(list(first_result))
 
-# second_result = (len(first[i]) == len(second[i]) for i in range(0,3))
+# second_result = (len(first[i]) == len(second[i]) for i in range(0,3)) - первоначальное решение
 
+# далее, доработанное решение с учетом комментария преподавателя
 second_result = (len(first[i]) == len(second[i]) for i in range(len(first))) if (len(first) <= len(second)) \
     else (len(first[i]) == len(second[i]) for i in range(len(second)))
 
